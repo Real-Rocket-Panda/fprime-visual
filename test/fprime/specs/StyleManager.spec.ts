d3bb0ea7de8a193252699408fe336bc92491fb70
miaoever <leo.miao.ever@gmail.com>
2018-06-12 10:40:19 -0400

    Add unit test for styleManager.

---
+++ test/fprime/specs/StyleManager.spec.ts
@@ -0,0 +1,17 @@
+import { expect } from "chai";
+import StyleManager from "../../../src/fprime/StyleManagement/StyleManager";
+
+describe("StyleManager", () => {
+    let styleManager: StyleManager;
+    before(() => {
+        styleManager = new StyleManager();
+    });
+
+    describe("check the format of the style file", ( ) => {
+        it("should load the defualt style file", () => {
+            const file = "/static/default.css";
+            const style = styleManager.getDefaultStyles(file);
+            expect(style.length).to.equal(3);
+        });
+    });
+});
