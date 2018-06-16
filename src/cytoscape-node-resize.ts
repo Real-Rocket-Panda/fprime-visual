e45214b169e3f638a45bcaabe64b8253428d16a4
Changjian Zhang <mljzcj@gmail.com>
2018-06-15 21:11:57 -0400

    Revert "Drag resize and API for resize by value input (#25)"
    This reverts commit d551446c44ac9e2d92df63580097a254794f6be3.

--- src/cytoscape-node-resize.ts
+++
@@ -1,1 +0,0 @@
-declare module "cytoscape-node-resize";
--- src/renderer/components/ViewContent/CyManager.ts
+++ src/renderer/components/ViewContent/CyManager.ts
@@ -1,6 +1,5 @@
 
 import { Cy_Util } from "./cyUtil";
-import { EventObject } from "cytoscape";
 class CyManager {
     public initialized: boolean = false;
 
@@ -19,18 +18,8 @@ class CyManager {
         this.cy_util = new Cy_Util(this.cy);
     }
 
-    public getGraph(): any {
-        return this.graph;
-    }
     public setGraph(graph: any): void {
         this.graph = graph;
-        // TODO: Move this logic to view descriptor
-        for (const comp of Object.keys(this.graph)) {
-            this.cy.$(comp).data("ports", this.graph[comp]);
-            this.graph[comp].forEach((port: any) => {
-                this.cy.$(port).data("component", comp);
-            });
-        }
     }
 
     /**
@@ -40,59 +29,8 @@ class CyManager {
         return this.cy.$(":selected");
     }
 
-    /**
-     * set a collection of elements to be a certain color
-     * @param eles collection of elements (implicitly of the same type)
-     * @param color value of color to change
-     */
     public setColor(eles: any, color: string): void {
         eles.style({ "background-color": color });
-    }
-
-    /**
-     * assign the width and height to a collection of elements
-     * @param eles collection of elements (implicitly of the same type)
-     * @param width the value of width to be set
-     * @param height the value of height to be set
-     */
-    public setSize(eles: any, width: number, height: number): void {
-        this.cy.batch(() => {
-            eles.array.forEach((node: any) => {
-                node.style({
-                    width: width,
-                    height: height,
-                });
-                if (node.is(".fprime-instance")) {
-                    this.cy_util.portMoveBackComp(node,
-                        ...this.graph["#" + node.id()]
-                            .map((port: any) => (this.cy.$(port))));
-                }
-            });
-        });
-    }
-
-
-    /**
-     * Enable the plugin nodeResize.
-     * Drag-to-resize only applies to the components.
-     * further options refer to:
-     * https://github.com/iVis-at-Bilkent/cytoscape.js-node-resize
-     */
-    public resize(): void {
-        this.cy.nodeResize({
-            isNoControlsMode: (node: any) => {
-                return !node.is(".fprime-instance");
-            },
-        });
-        this.cy.on("noderesize.resizeend",
-            (evt: EventObject, type: any, node: any) => {
-                /* type param includes:
-                topleft, topcenter, topright, centerright,
-                bottomright, bottomcenter, bottomleft, centerleft
-                */
-                this.cy_util.portMoveBackComp(node, ...node.data("ports").map(
-                    (p: string) => this.cy.$(p)));
-            });
     }
 
     /**
@@ -119,8 +57,8 @@ class CyManager {
                 animationEasing: "ease-out",
                 animationDuration: 0,
                 stop: () => {
-                    this.stickPort();
-                    this.movebackPort();
+                     this.stickPort();
+                     this.movebackPort();
                 },
             });
             layout.run();
@@ -132,8 +70,8 @@ class CyManager {
             const layout: any = this.cy.layout({
                 name: "preset",
                 stop: () => {
-                    this.stickPort();
-                    this.movebackPort();
+                     this.stickPort();
+                     this.movebackPort();
                 },
             });
             layout.run();
@@ -144,30 +82,18 @@ class CyManager {
         for (const comp of Object.keys(this.graph)) {
             this.cy_util.portMoveWizComp(this.cy.$(comp),
                 this.graph[comp].join(","));
+            this.cy_util.portStick2Comp(
+                this.cy.$(comp),
+                ...this.graph[comp].map((k: any) => this.cy.$(k)),
+            );
         }
-
-        this.cy.nodes(".fprime-port").on("drag", (evt: EventObject) => {
-            const portIns = evt.target;
-            const compIns = this.cy.$(evt.target.data("component"));
-            this.cy_util.positionInBox(
-                portIns.position(),
-                this.cy_util.generateBox(
-                    compIns.boundingBox({includeOverlays: false}),
-                    portIns.width(),
-                    portIns.height()));
-
-            this.cy_util.positionOutBox(
-                portIns.position(),
-                compIns.boundingBox({includeOverlays: false}));
-        });
     }
 
     private movebackPort(): void {
         for (const comp of Object.keys(this.graph)) {
-            const compIns = this.cy.$(comp);
             this.cy_util.portMoveBackComp(
-                compIns,
-                ...compIns.data("ports").map((k: any) => this.cy.$(k)),
+                this.cy.$(comp),
+                ...this.graph[comp].map((k: any) => this.cy.$(k)),
             );
         }
     }
