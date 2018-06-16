ab33a0d3c53235778717b1bbe86002a9089d77e3
Changjian Zhang <mljzcj@gmail.com>
2018-06-15 21:04:21 -0400

    Revert "Fix the static path bug of configuration file (#32)"
    This reverts commit 607bd98e8e88966bc793ebf995c1f477f5003471.

--- src/fprime/ConfigManagement/ConfigManager.ts
+++ src/fprime/ConfigManagement/ConfigManager.ts
@@ -1,26 +1,19 @@
 import * as fs from "fs";
+import * as path from "path";
+import IConfig from "../Common/Config";
 
 export default class ConfigManager {
-    private config: IConfig = {
-        FPPCompilerPath: "",
-        FPPCompilerOutputPath: "",
-        DefaultStyleFilePath: "",
-        Analyzers: [],
-    };
+    private config: IConfig;
+    constructor() {
+        const obj = fs.readFileSync(
+            path.resolve(__dirname, "../../../static/config.json"),
+            "utf-8",
+        );
+        this.config = JSON.parse(obj);
+    }
 
     public getConfig(): IConfig {
         return this.config;
-    }
-
-    /**
-     * Load the project config from a given absolute path.
-     * @param dir The absolute path of the project directory
-     */
-    public loadConfig(dir: string) {
-        if (fs.existsSync(dir)) {
-            const obj = fs.readFileSync(dir, "utf-8");
-            this.config = JSON.parse(obj);
-        }
     }
 
 }
