38e13cb272d6d2d79cabad326954cc8dc122ea38
spade1015 <39559161+spade1015@users.noreply.github.com>
2019-07-08 18:04:15 -0400

    Update package dependency

--- src/renderer/components/MessageFooter.vue
+++ src/renderer/components/MessageFooter.vue
@@ -1,52 +1,65 @@
 <template>
-  <v-footer app fixed :height="height" style="min-height: 20px">
-    <v-btn
-      flat
-      small
-      class="msg-footer-btn"
-      :style="{ height: height + 'px' }"
-      id="show-panel-output"
-      @click="showOutput"
-    >
-      Output
-    </v-btn>
-    <v-btn
-      flat
-      small
-      class="msg-footer-btn"
-      :style="{ height: height + 'px' }"
-      id="show-panel-analysis"
-      @click="showAnalysis"
-    >
-      Analysis
-    </v-btn>
-    <v-spacer></v-spacer>
-    <span class="mr-2 caption">&copy; 2019 The Jet Trio</span>
-  </v-footer>
+    <v-footer app fixed :height="height" style="min-height: 20px">
+        <v-btn
+                flat
+                small
+                class="msg-footer-btn"
+                :style="{ height: height + 'px' }"
+                id="show-panel-output"
+                @click="showOutput"
+        >
+            Output
+        </v-btn>
+        <v-btn
+                flat
+                small
+                class="msg-footer-btn"
+                :style="{ height: height + 'px' }"
+                id="show-panel-analysis"
+                @click="showAnalysis"
+        >
+            Analysis
+        </v-btn>
+        <v-btn
+                flat
+                small
+                class="msg-footer-btn"
+                :style="{ height: height + 'px' }"
+                id="show-panel-editor"
+                @click="showEditor"
+        >
+            Editor
+        </v-btn>
+        <v-spacer></v-spacer>
+        <span class="mr-2 caption">&copy; 2019 The Jet Trio</span>
+    </v-footer>
 </template>
 
 <script lang="ts">
-import Vue from "vue";
-import panel from "@/store/panel";
+    import Vue from "vue";
+    import panel from "@/store/panel";
 
-export default Vue.extend({
-  props: ["height"],
-  name: "message-footer",
-  methods: {
-    showOutput() {
-      panel.showOutput();
-    },
-    showAnalysis() {
-      panel.showAnalysis();
-    }
-  }
-});
+    export default Vue.extend({
+        props: ["height"],
+        name: "message-footer",
+        methods: {
+            showOutput() {
+                panel.showOutput();
+            },
+            showAnalysis() {
+                panel.showAnalysis();
+            },
+            showEditor() {
+                panel.showEditor();
+            }
+        }
+    });
 </script>
 
 <style>
-.msg-footer-btn {
-  text-transform: none;
-  margin: 0px;
-}
+    .msg-footer-btn {
+        text-transform: none;
+        margin: 0px;
+    }
 </style>
 
